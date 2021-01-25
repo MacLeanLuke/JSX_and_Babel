de8d98aae3a14130b8d5b571b5b3e72d3c9299cb
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-01-24 19:06:57 -0500

    Add the Brain's ToDo List with JSX

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(
+<div>
+  <h1>Tonight's Todo's...</h1>
+  <p>The same thing that we try to do every night. Try to take over the world.</p>
+</div>, 
+  document.getElementById("root")
+);
